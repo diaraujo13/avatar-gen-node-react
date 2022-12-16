e7dabcbc787f6ae5443d66f7ae683e518017aad6
Izaías Araújo <izaias.araujo@univasf.edu.br>
2022-12-15 21:57:17 -0300

    :rocket: initial files

---
+++ api/index.ts
@@ -0,0 +1,16 @@
+import express, { Express, Request, Response } from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app: Express = express();
+const port: number = Number(process.env.PORT) || 3000;
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+
+    console.log(`🌟 Server UP in ${port}`);
+})
